9f45d59665f5a2a19b9a48320250a28686ee6982
andi aguirre <darckisgamer@gmail.com>
2025-03-23 14:59:39 -0500

    Agregar precio opcional al componente CardMenu y ajustar estilos

--- src/components/CardMenu.tsx
+++ src/components/CardMenu.tsx
@@ -1,14 +1,18 @@
 import { MdAddShoppingCart } from "react-icons/md";
-import { CartaType } from "../const/Carta";
 
-const CardMenu = ({ title, image }: CartaType) => {
+interface CartaType {
+  title: string;
+  image: string;
+  price?: number;
+}
+const CardMenu = ({ title, image, price }: CartaType) => {
   return (
     <li className="group cursor-pointer">
       <div className="relative">
         <img
           src={image}
           alt=""
-          className="aspect-square object-center object-cover"
+          className="aspect-square object-center object-cover w-full"
         />
         <div className=" w-full h-full bg-black/30 bg-opacity-50 absolute top-0 left-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity">
           <span className="text-white text-3xl">
@@ -20,6 +24,11 @@ const CardMenu = ({ title, image }: CartaType) => {
       <h3 className="text-[#2D3A4B] uppercase text-lg text-center group-hover:text-yellow-500">
         {title}
       </h3>
+      {price && (
+        <span className="text-center block text-red-700 font-bold ">
+          S/{price}.00
+        </span>
+      )}
     </li>
   );
 };
